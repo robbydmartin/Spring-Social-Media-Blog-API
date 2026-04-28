3734eb0a948982ff5697536af68f46444fd72482
Robby Martin <142545854+robbydmartin@users.noreply.github.com>
2026-04-28 09:06:57 -0400

    Remove comment from SocialMediaController
    Removed TODO comment regarding endpoint implementation.

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -17,12 +17,6 @@ import com.example.entity.*;
 import com.example.service.*;
 import com.example.exception.*;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 @RestController
 @ControllerAdvice
 public class SocialMediaController {
